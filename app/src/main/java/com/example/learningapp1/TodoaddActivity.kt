f6ab750032289369fb43721e2e02f7daecec9217
Muhamad Azis <muhamadazis9a@gmail.com>
2022-02-04 18:50:14 +0700

    add app

---
+++ app/src/main/java/com/example/learningapp1/TodoaddActivity.kt
@@ -0,0 +1,41 @@
+package com.example.learningapp1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class TodoaddActivity : AppCompatActivity() {
+
+    lateinit var todoDBHelper2: DBHelper2
+    lateinit var inputJudul: EditText
+    lateinit var inputTanggal: EditText
+    lateinit var inputKeterangan: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todoadd)
+
+        inputJudul = findViewById(R.id.input_judul)
+        inputTanggal = findViewById(R.id.input_tanggal)
+        inputKeterangan = findViewById(R.id.input_keterangan)
+        todoDBHelper2 = DBHelper2(this)
+
+    }
+    fun addData(v: View){
+        var judulin = inputJudul.text.toString()
+        var tanggalin = inputTanggal.text.toString()
+        var keteranganin = inputKeterangan.text.toString()
+        todoDBHelper2.insertData(judulin, tanggalin, keteranganin)
+        inputJudul.setText("")
+        inputTanggal.setText("")
+        inputKeterangan.setText("")
+
+    }
+    fun showAll(v: View){
+        var pindah = Intent(this, TodoActivity::class.java)
+        startActivity(pindah)
+    }
+
+}
