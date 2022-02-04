f1420bbfe681bd3b3361c88f8ed0cccc2aa6ab51
Muhamad Azis <muhamadazis9a@gmail.com>
2022-02-04 22:18:22 +0700

    Halaman utama

--- app/src/main/java/com/example/learningapp1/MainActivity.kt
+++ app/src/main/java/com/example/learningapp1/MainActivity.kt
@@ -4,25 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnLogout: Button = findViewById(R.id.btnLogout)
-        val btnList: Button = findViewById(R.id.btnList)
+
+        val cvData: CardView = findViewById(R.id.cv_listData)
+        val cvAdd: CardView = findViewById(R.id.cv_addData)
+        //val cvAbout: CardView = findViewById(R.id.cv_about)
+        val cvLogout: CardView = findViewById(R.id.cv_logout)
+
         val savedLogin = getSharedPreferences("Login", MODE_PRIVATE)
         val editSavedLogin = savedLogin.edit()
-        btnLogout.setOnClickListener {
+
+        cvData.setOnClickListener {
+            var pindah1 = Intent(this, RvDbActivity::class.java)
+            startActivity(pindah1)
+        }
+        cvAdd.setOnClickListener {
+            var pindah2 = Intent(this, TodoaddActivity::class.java)
+            startActivity(pindah2)
+        }
+
+        cvLogout.setOnClickListener {
             editSavedLogin.putString("Email", null)
             editSavedLogin.putString("Password", null)
             editSavedLogin.putString("Status", "Off")
             editSavedLogin.commit()
             startActivity(Intent(this, LoginActivity::class.java))
         }
-        btnList.setOnClickListener {
-            var pindah = Intent(this, TodoaddActivity::class.java)
-            startActivity(pindah)
-        }
+
+
+
     }
 }
